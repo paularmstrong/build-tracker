ed26c4b2260428a722c26b2316ecdc6560b35302
Paul Armstrong <paul@spaceyak.com>
2017-08-05 15:07:02 -0700

    use AreaChart on Home

--- packages/webapp/src/Home.js
+++ packages/webapp/src/Home.js
@@ -1,49 +1,14 @@
-import { select } from 'd3-selection';
+import AreaChart from './charts/AreaChart';
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-const CHART_ASPECT_RATIO = 4 / 3;
-
 export default class Home extends Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      width: 0,
-      height: 0
-    };
-  }
-
-  componentDidMount() {
-    this._renderChart();
-  }
-
-  componentDidUpdate() {
-    this._renderChart();
-  }
-
   render() {
-    const { height, width } = this.state;
     return (
-      <View onLayout={this._handleLayout} style={{ height: '100%' }}>
+      <View style={{ height: '100%' }}>
         Home
-        <svg ref={this._setSvgRef} width={width} height={height} />
+        <AreaChart />
       </View>
     );
   }
-
-  _renderChart() {
-    select(this._svg);
-  }
-
-  _setSvgRef = node => {
-    this._svg = node;
-  };
-
-  _handleLayout = event => {
-    const { nativeEvent: { layout: { width, height: layoutHeight } } } = event;
-    if (width !== this.state.width) {
-      const height = Math.min(layoutHeight, width / CHART_ASPECT_RATIO);
-      this.setState({ height, width });
-    }
-  };
 }

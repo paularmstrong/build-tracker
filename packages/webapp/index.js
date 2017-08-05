d4c12e12e45eea3aba50b09fa6d7b182da4602ee
Paul Armstrong <paul@spaceyak.com>
2017-08-04 19:08:13 -0700

    ensure index is served for all routes

--- packages/webapp/index.js
+++ packages/webapp/index.js
@@ -5,4 +5,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
 app.listen(3000);

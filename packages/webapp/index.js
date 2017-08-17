338c4e5d9841674c0d3cafe1c32ec26420999a4a
Paul Armstrong <paul@spaceyak.com>
2017-08-16 20:04:54 -0700

    add message for starting express server

--- packages/webapp/index.js
+++ packages/webapp/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -9,4 +10,5 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
-app.listen(3000);
+app.listen(port);
+console.log(`Application running at http://localhost:${port}`);

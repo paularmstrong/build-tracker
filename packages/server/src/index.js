a3b6e48f74d04904d5b5901d58f49723d9824f5a
Paul Armstrong <paul@spaceyak.com>
2017-10-27 13:47:09 -0700

    validate post data before inserting

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,4 +1,5 @@
 // @flow
+import assert from 'assert';
 import bodyParser from 'body-parser';
 import express from 'express';
 import glob from 'glob';
@@ -24,17 +25,63 @@ export type ServerOptions = {
   port?: number
 };
 
+export type NormalizedQuery = {
+  startTime?: number,
+  endTime?: number,
+  count?: number
+};
+
+const normalizeQuery = (query: {}): NormalizedQuery => {
+  return Object.keys(query).reduce((memo: NormalizedQuery, key) => {
+    const value = query[key];
+    switch (key) {
+      case 'startTime':
+      case 'endTime':
+      case 'count':
+        memo[key] = parseInt(value, 10);
+        break;
+      case 'startRevision':
+      case 'endRevision':
+        memo[key] = `${value}`;
+        break;
+      // no default
+    }
+    return memo;
+  }, {});
+};
+
+const isValidBuild = data => {
+  assert(data.meta && typeof data.meta === 'object', 'Metadata is provided');
+  assert(data.meta.revision && typeof data.meta.revision === 'string', 'Build revision is provided');
+  assert(data.meta.timestamp && typeof data.meta.timestamp === 'number', 'Build timestamp is provided');
+  assert(data.stats && typeof data.stats === 'object', 'Build stats are provided');
+  Object.keys(data.stats).forEach(key => {
+    const artifact = data.stats[key];
+    assert(artifact.name, `Name is provided for artifact ${key}`);
+    assert(artifact.size, `Size is provided for artifact ${key}`);
+  });
+};
+
 export default function createServer({ getBuilds, insertBuild, port = 3000 }: ServerOptions) {
   app.get('/api/builds', (req: $Request, res: $Response) => {
-    getBuilds(req.query).then(data => {
-      console.log('got data', data.length);
+    getBuilds(normalizeQuery(req.query)).then(data => {
       res.write(JSON.stringify(data));
       res.end();
     });
   });
 
   app.post('/api/builds', (req: $Request, res: $Response) => {
-    insertBuild(req.body)
+    const build = req.body;
+    try {
+      isValidBuild(build);
+    } catch (err) {
+      res.status(400);
+      res.write(JSON.stringify({ success: false, error: err.toString() }));
+      res.end();
+      return;
+    }
+
+    insertBuild(build)
       .then(() => {
         res.write(JSON.stringify({ success: true }));
         res.end();
@@ -47,7 +94,7 @@ export default function createServer({ getBuilds, insertBuild, port = 3000 }: Se
   });
 
   app.get('*', (req: $Request, res: $Response) => {
-    res.sendFile(path.join(APP_HTML_PATH));
+    res.sendFile(APP_HTML_PATH);
   });
 
   app.listen(port);
@@ -60,7 +107,7 @@ export type StaticServerOptions = {
 };
 
 export const staticServer = ({ port, statsRoot }: StaticServerOptions) => {
-  const getBuilds = () => {
+  const getBuilds = args => {
     return new Promise((resolve, reject) => {
       glob(`${statsRoot}/*.json`, (err, matches) => {
         if (err) {

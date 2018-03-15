2fcea15afd5f9eaf3691af62b7aa89fc44bc0c39
Paul Armstrong <paul@spaceyak.com>
2018-03-15 15:25:28 -0700

    [app] add table row hovers, fix artifact switch links

--- packages/app/src/components/ComparisonTable/ValueCell.js
+++ packages/app/src/components/ComparisonTable/ValueCell.js
@@ -6,6 +6,8 @@ import { Text } from 'react-native';
 import React, { PureComponent } from 'react';
 
 type Props = {
+  hoverColor: string,
+  isHovered: boolean,
   stat: number,
   gzip: number,
   valueType: 'gzip' | 'stat'
@@ -13,10 +15,10 @@ type Props = {
 
 export default class ValueCell extends PureComponent<Props> {
   render() {
-    const { valueType } = this.props;
+    const { hoverColor, isHovered, valueType } = this.props;
     const value = this.props[valueType];
     return (
-      <Td style={[styles.cell]}>
+      <Td style={[styles.cell, isHovered && { backgroundColor: hoverColor }]}>
         <Text>{value ? bytesToKb(value) : '-'}</Text>
       </Td>
     );

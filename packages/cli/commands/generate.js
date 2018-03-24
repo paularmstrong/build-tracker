bdc1e241d16e7fb23fb77c95a141ed1abbe5f587
Paul Armstrong <paul@spaceyak.com>
2018-03-24 09:03:13 -0700

    [cli] Add build stat generation command
    Fixes #13

---
+++ packages/cli/commands/generate.js
@@ -0,0 +1,75 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const glob = require('glob');
+const gzip = require('gzip-size');
+const path = require('path');
+
+exports.command = 'generate';
+
+exports.describe = 'Generate stats for a build';
+
+const usage = `Usage: $0 ${exports.command} [options]
+
+${exports.describe}.`;
+
+exports.builder = yargs =>
+  yargs
+    .usage(usage)
+    .option('dir', {
+      alias: 'd',
+      describe: 'Directory to find artifacts',
+      demandOption: true,
+      normalize: true,
+      type: 'string'
+    })
+    .option('glob', {
+      alias: 'g',
+      describe: 'Glob string to match artifacts',
+      demandOption: true,
+      type: 'array'
+    })
+    .option('filename-template', {
+      alias: 'f',
+      describe: 'Regular expression for matching the name of the file',
+      type: 'string'
+    })
+    .option('meta.sha', {
+      describe: 'SHA of the build',
+      type: 'string'
+    })
+    .option('meta.branch', {
+      describe: 'Branch of the build',
+      type: 'string'
+    })
+    .option('pretty', {
+      alias: 'p',
+      describe: 'Pretty-print the JSON',
+      type: 'boolean'
+    });
+
+exports.handler = argv => {
+  const filenameMatcher = argv['filename-template'] ? new RegExp(argv['filename-template']) : /(.+)\.[^.]+$/;
+  const assets = [].concat.apply([], argv.glob.map(pattern => glob.sync(`${argv.dir}/${pattern}`)));
+  const artifacts = assets.reduce((artifacts, assetPath) => {
+    const source = fs.readFileSync(assetPath);
+    const hash = crypto.createHash('md5');
+    const assetFilename = path.basename(assetPath);
+    const name = assetFilename.match(filenameMatcher);
+    const matchedName = name[1];
+    return Object.assign({}, artifacts, {
+      [matchedName]: {
+        hash: hash.update(source).digest('hex'),
+        name: matchedName,
+        stat: Buffer.byteLength(source, 'utf8'),
+        gzip: gzip.sync(source)
+      }
+    });
+  }, {});
+
+  const build = {
+    meta: argv.meta,
+    artifacts
+  };
+
+  console.log(JSON.stringify(build, null, argv.pretty ? 2 : 0));
+};

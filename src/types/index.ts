5a8ebe9c5b82a74e840b7d2fb30cb19f60505239
Paul Armstrong <paul@spaceyak.com>
2019-03-04 19:18:22 -0800

    [chore] type comments

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,9 +4,28 @@
 export type ArtifactFilters = Array<RegExp>;
 
 export interface Budget {
+  /**
+   * Budget callout level
+   * warn - inform that this change could be problematic
+   * error - this change violates the budget and should not be accepted
+   */
   level: 'warn' | 'error';
+  /**
+   * Type of file size to operate on. Usually 'gzip' or 'stat1'
+   * @type {string}
+   */
   sizeKey: string;
+  /**
+   * Type of change to moniro
+   * delta - maximum allowed for new value minus previous value
+   * percentDelta - maximum allowed for the percent change from the previous value to the new value
+   * size - absolute maximum value
+   */
   type: 'delta' | 'percentDelta' | 'size';
+  /**
+   * Maximum value for the type defined above
+   * @type {number}
+   */
   maximum: number;
 }
 

fddffd6bc50304951b9a94b3d9e758d73cf9c17c
Paul Armstrong <paul@spaceyak.com>
2019-03-24 08:20:13 -0700

    [app] add branch tests for textfield

--- src/app/src/components/__tests__/TextField.test.tsx
+++ src/app/src/components/__tests__/TextField.test.tsx
@@ -1,12 +1,34 @@
 /**
  * Copyright (c) 2019 Paul Armstrong
  */
+import HeartIcon from '../../icons/Heart';
 import React from 'react';
 import TextField from '../TextField';
 import { TextInput } from 'react-native';
 import { fireEvent, render } from 'react-native-testing-library';
 
 describe('TextField', () => {
+  describe('leadingIcon', () => {
+    test('renders an icon', () => {
+      const { queryAllByType } = render(<TextField label="tacos" leadingIcon={HeartIcon} />);
+      expect(queryAllByType(HeartIcon)).toHaveLength(1);
+    });
+  });
+
+  describe('trailingIcon', () => {
+    test('renders an icon', () => {
+      const { queryAllByType } = render(<TextField label="tacos" trailingIcon={HeartIcon} />);
+      expect(queryAllByType(HeartIcon)).toHaveLength(1);
+    });
+  });
+
+  describe('helpText', () => {
+    test('renders the help text', () => {
+      const { queryAllByText } = render(<TextField label="tacos" helpText="this is help" />);
+      expect(queryAllByText('this is help')).toHaveLength(1);
+    });
+  });
+
   describe('onBlur', () => {
     test('is fired for when the TextInput is blurred', () => {
       const handleBlur = jest.fn();

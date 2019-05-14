a22f910477998299b4613d90f21eb41f51f8b82d
Paul Armstrong <paul@spaceyak.com>
2019-05-13 17:44:41 -0700

    [docs] fix repoUrl

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -83,7 +83,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  repoUrl: 'https://github.com/paularmstrong/build-tracker/docs'
+  repoUrl: 'https://github.com/paularmstrong/build-tracker'
 };
 
 module.exports = siteConfig;

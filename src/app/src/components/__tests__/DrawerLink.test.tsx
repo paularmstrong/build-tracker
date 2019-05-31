f1167c17c6b30e5e9e50902f8c4fbe43765ba67c
Paul Armstrong <paul@spaceyak.com>
2019-05-31 13:51:51 -0700

    [chore] convert DrawerLink.test to @testing-library/react

--- src/app/src/components/__tests__/DrawerLink.test.tsx
+++ src/app/src/components/__tests__/DrawerLink.test.tsx
@@ -1,26 +1,30 @@
 /**
  * Copyright (c) 2019 Paul Armstrong
  */
-import * as Theme from '../../theme';
 import DrawerLink from '../DrawerLink';
 import HeartIcon from '../../icons/Heart';
 import React from 'react';
-import { fireEvent, render } from 'react-native-testing-library';
-import { StyleSheet, Text } from 'react-native';
+import { fireEvent, render } from '@testing-library/react';
 
 describe('DrawerLink', () => {
   describe('icon', () => {
     test('does not render an icon', () => {
-      const { getByType } = render(<DrawerLink href="https://build-tracker.local" text="tacos" />);
-      expect(getByType(Text).props.children.props.children).toHaveLength(2);
-      expect(getByType(Text).props.children.props.children).toEqual([null, 'tacos']);
+      const { getByRole } = render(<DrawerLink href="https://build-tracker.local" text="tacos" />);
+      expect(getByRole('link').firstChild).toMatchInlineSnapshot(`
+        <div
+          class="css-text-76zvg2"
+          dir="auto"
+          style="font-size: 1rem;"
+        >
+          tacos
+        </div>
+      `);
     });
 
     test('renders the icon', () => {
-      const { queryAllByType } = render(
-        <DrawerLink href="https://build-tracker.local" icon={HeartIcon} text="tacos" />
-      );
-      expect(queryAllByType(HeartIcon)).toHaveLength(1);
+      const { getByRole } = render(<DrawerLink href="https://build-tracker.local" icon={HeartIcon} text="tacos" />);
+      // @ts-ignore
+      expect(getByRole('link').firstChild.firstChild.tagName).toEqual('svg');
     });
   });
 
@@ -30,27 +34,21 @@ describe('DrawerLink', () => {
     });
 
     test('adds a bg color', () => {
-      const { getByProps } = render(<DrawerLink href="https://build-tracker.local" text="tacos" />);
-      const root = getByProps({ accessibilityRole: 'link' });
-      expect(StyleSheet.flatten(root.props.style)).not.toMatchObject({
-        backgroundColor: expect.any(String)
-      });
-      fireEvent(root, 'mouseEnter');
-      expect(StyleSheet.flatten(root.props.style)).toMatchObject({
-        backgroundColor: Theme.Color.Primary00
-      });
+      const { getByRole } = render(<DrawerLink href="https://build-tracker.local" text="tacos" />);
+      const link = getByRole('link');
+      expect(link.style.backgroundColor).toEqual('');
+      fireEvent.mouseEnter(link);
+      expect(link.style.backgroundColor).toEqual('rgb(199, 235, 255)');
     });
 
     test('changes font color', () => {
-      const { getByType } = render(<DrawerLink href="https://build-tracker.local" text="tacos" />);
-      const root = getByType(Text);
-      expect(StyleSheet.flatten(root.props.style)).not.toMatchObject({
-        color: expect.any(String)
-      });
-      fireEvent(root, 'mouseEnter');
-      expect(StyleSheet.flatten(root.props.style)).toMatchObject({
-        color: Theme.Color.Primary40
-      });
+      const { getByRole } = render(<DrawerLink href="https://build-tracker.local" text="tacos" />);
+      const link = getByRole('link');
+      // @ts-ignore
+      expect(link.firstChild.style.color).toEqual('');
+      fireEvent.mouseEnter(link);
+      // @ts-ignore
+      expect(link.firstChild.style.color).toEqual('rgb(18, 52, 108)');
     });
   });
 });

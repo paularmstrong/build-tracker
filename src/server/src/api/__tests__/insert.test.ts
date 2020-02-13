a926b68399171beeb777df113b07e9244cb292ab
Oliver Joseph Ash <oliverjash@gmail.com>
2020-02-13 18:51:09 +0000

    Add tests

--- src/server/src/api/__tests__/insert.test.ts
+++ src/server/src/api/__tests__/insert.test.ts
@@ -6,6 +6,7 @@ import Build from '@build-tracker/build';
 import Comparator from '@build-tracker/comparator';
 import express from 'express';
 import { insertBuild } from '../insert';
+import { NotFoundError } from '@build-tracker/api-errors';
 import request from 'supertest';
 
 const build = new Build({ branch: 'master', revision: 'abc', parentRevision: 'def', timestamp: Date.now() }, [
@@ -65,27 +66,103 @@ describe('insert build handler', () => {
     });
   });
 
-  describe('onInserted', () => {
-    test('called with a comparator of the inserted branch against its parentRevision', () => {
-      const handleInsert = jest.fn(() => Promise.resolve());
-      const handler = insertBuild(queries, config, handleInsert);
-      app.post('/test', handler);
+  describe('parent revision', () => {
+    describe('onInserted', () => {
+      test('called with a comparator of the inserted branch against its parentRevision', () => {
+        const handleInsert = jest.fn(() => Promise.resolve());
+        const handler = insertBuild(queries, config, handleInsert);
+        app.post('/test', handler);
 
-      return request(app)
-        .post('/test')
-        .send({ meta: build.meta, artifacts: build.artifacts })
-        .set('Content-Type', 'application/json')
-        .set('Accept', 'application/json')
-        .then(() => {
-          expect(handleInsert).toHaveBeenCalled();
-          // @ts-ignore
-          const [comparator] = handleInsert.mock.calls[0];
-          // @ts-ignore
-          expect(comparator.builds.map(build => build.getMetaValue('revision'))).toEqual([
-            build.getMetaValue('revision'),
-            parentBuild.getMetaValue('revision')
-          ]);
-        });
+        return request(app)
+          .post('/test')
+          .send({ meta: build.meta, artifacts: build.artifacts })
+          .set('Content-Type', 'application/json')
+          .set('Accept', 'application/json')
+          .then(() => {
+            expect(handleInsert).toHaveBeenCalled();
+            // @ts-ignore
+            const [comparator] = handleInsert.mock.calls[0];
+            // @ts-ignore
+            expect(comparator.builds.map(build => build.getMetaValue('revision'))).toEqual([
+              build.getMetaValue('revision'),
+              parentBuild.getMetaValue('revision')
+            ]);
+          });
+      });
+    });
+  });
+
+  describe('no parent revision', () => {
+    let app, config, queries;
+    beforeEach(() => {
+      queries = {
+        byRevision: jest.fn(() => Promise.reject(new NotFoundError('TODO'))),
+        insert: jest.fn(() => Promise.resolve('1234'))
+      };
+      config = {
+        artifacts: {},
+        queries
+      };
+      app = express();
+      app.use(bodyParser.json());
+    });
+
+    describe('insert', () => {
+      test('inserts the build', () => {
+        const handler = insertBuild(queries, config);
+        app.post('/test', handler);
+
+        return request(app)
+          .post('/test')
+          .send({ meta: build.meta, artifacts: build.artifacts })
+          .set('Content-Type', 'application/json')
+          .set('Accept', 'application/json')
+          .then(() => {
+            expect(queries.insert).toHaveBeenCalled();
+          });
+      });
+    });
+
+    describe('response', () => {
+      test('returns a JSON serialized Comparator and summary', () => {
+        const handler = insertBuild(queries, config);
+        app.post('/test', handler);
+
+        const comparator = new Comparator({ builds: [build] });
+
+        return request(app)
+          .post('/test')
+          .send({ meta: build.meta, artifacts: build.artifacts })
+          .set('Content-Type', 'application/json')
+          .set('Accept', 'application/json')
+          .then(res => {
+            expect(res.body.comparatorData).toEqual(comparator.serialize());
+            expect(res.body.summary).toEqual(comparator.toSummary());
+          });
+      });
+    });
+
+    describe('onInserted', () => {
+      test('called with a comparator of the inserted branch against its parentRevision', () => {
+        const handleInsert = jest.fn(() => Promise.resolve());
+        const handler = insertBuild(queries, config, handleInsert);
+        app.post('/test', handler);
+
+        return request(app)
+          .post('/test')
+          .send({ meta: build.meta, artifacts: build.artifacts })
+          .set('Content-Type', 'application/json')
+          .set('Accept', 'application/json')
+          .then(() => {
+            expect(handleInsert).toHaveBeenCalled();
+            // @ts-ignore
+            const [comparator] = handleInsert.mock.calls[0];
+            // @ts-ignore
+            expect(comparator.builds.map(build => build.getMetaValue('revision'))).toEqual([
+              build.getMetaValue('revision')
+            ]);
+          });
+      });
     });
   });
 });

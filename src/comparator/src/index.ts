fd91fa3de16c4732fffedd43a99880c75bd9f042
Oliver Joseph Ash <oliverjash@gmail.com>
2020-02-17 12:49:05 +0000

    Handle 0 rows

--- src/comparator/src/index.ts
+++ src/comparator/src/index.ts
@@ -411,7 +411,9 @@ export default class BuildComparator {
     const groups = this.getStringFormattedGroups(formatTotal, formatDelta, sizeKey);
     const rows = this.getStringFormattedRows(formatTotal, formatDelta, sizeKey, artifactFilter);
 
-    return markdownTable([header, ...groups, ...rows], { align: rows[0].map((_, i) => (i === 0 ? 'l' : 'r')) });
+    const firstRow = rows[0] !== undefined ? rows[0] : [];
+
+    return markdownTable([header, ...groups, ...rows], { align: firstRow.map((_, i) => (i === 0 ? 'l' : 'r')) });
   }
 
   public toCsv({

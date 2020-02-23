418add10f9206b0a453d38d7c7cc29fe643be91d
Paul Armstrong <paul@spaceyak.com>
2020-02-23 09:23:24 -0800

    [app] limit number of tick labels to prevent overlap

--- src/app/src/components/Graph/__tests__/XAxis.test.tsx
+++ src/app/src/components/Graph/__tests__/XAxis.test.tsx
@@ -8,12 +8,11 @@ import { render } from '@testing-library/react';
 import { scalePoint } from 'd3-scale';
 import XAxis from '../XAxis';
 
-const scale = scalePoint()
-  .range([0, 100])
-  .domain(['1234567abcdef', 'abcdefg1234567', 'abcd', '1234']);
-
 describe('XAxis', () => {
   test('creates a bottom axis', () => {
+    const scale = scalePoint()
+      .range([0, 100])
+      .domain(['1234567abcdef', 'abcdefg1234567', 'abcd', '1234']);
     const mockCall = jest.fn(() => {
       selectSpy.mockRestore();
       return Selection.select(document.createElement('g'));
@@ -31,6 +30,9 @@ describe('XAxis', () => {
   });
 
   test('formats shas', () => {
+    const scale = scalePoint()
+      .range([0, 100])
+      .domain(['1234567abcdef', 'abcdefg1234567', 'abcd', '1234']);
     const mockTickFormat = jest.fn(() => {
       axisSpy.mockRestore();
       return Axes.axisBottom(scale);
@@ -49,5 +51,35 @@ describe('XAxis', () => {
     const formatter = mockTickFormat.mock.calls[0][0];
     // @ts-ignore
     expect(formatter('1234567abcdef')).toEqual('1234567');
+  });
+
+  test('limits number of tick labels', () => {
+    const domain = new Array(150).fill(0).map((_v, i) => `12345${i}`);
+    const scale = scalePoint()
+      .range([0, 100])
+      .domain(domain);
+    const mockTickFormat = jest.fn(() => {
+      axisSpy.mockRestore();
+      return Axes.axisBottom(scale);
+    });
+    // @ts-ignore Just want to make sure call is called
+    const axisSpy = jest.spyOn(Axes, 'axisBottom').mockReturnValue({ tickFormat: mockTickFormat });
+
+    render(
+      <svg>
+        <XAxis height={400} scale={scale} />
+      </svg>
+    );
+
+    // @ts-ignore
+    const formatter: (string, number) => string = mockTickFormat.mock.calls[0][0];
+
+    expect(formatter('1234567abc', 0)).toEqual('1234567');
+    expect(formatter('1234567abc', 1)).toEqual('');
+    expect(formatter('1234567abc', 2)).toEqual('');
+    expect(formatter('1234567abc', 3)).toEqual('');
+    expect(formatter('1234567abc', 4)).toEqual('');
+    expect(formatter('1234567abc', 5)).toEqual('');
+    expect(formatter('1234567abc', 6)).toEqual('1234567');
   });
 });

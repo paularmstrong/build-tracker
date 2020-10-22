80d0094ec60831da0e1761c08c533a6de10a6d88
Ralph Cowling <rcowling@twitter.com>
2020-10-22 16:44:57 +0100

    s/builds/build

--- src/api-client/src/modules/__tests__/get-build.test.ts
+++ src/api-client/src/modules/__tests__/get-build.test.ts
@@ -17,13 +17,13 @@ describe('getBuild', () => {
   });
 
   test('when found returns build', async () => {
-    nock(`${config.applicationUrl}/`).get('/api/builds/1bf811ec249c2b13a314e127312b2d760f6658e2').reply(200, fakeBuild);
+    nock(`${config.applicationUrl}/`).get('/api/build/1bf811ec249c2b13a314e127312b2d760f6658e2').reply(200, fakeBuild);
 
     await expect(getBuild(config, '1bf811ec249c2b13a314e127312b2d760f6658e2')).resolves.toMatchObject(fakeBuild);
   });
 
   test('when missing', async () => {
-    nock(`${config.applicationUrl}/`).get('/api/builds/blah').reply(404, new NotFoundError());
+    nock(`${config.applicationUrl}/`).get('/api/build/blah').reply(404, new NotFoundError());
 
     await expect(getBuild(config, 'blah')).rejects.toThrow(Error);
   });
